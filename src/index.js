354f927a907d8317de363072308ce06f7dfa0e87
Mariana Lobo <m.lobocasas@gmail.com>
2023-03-31 11:36:46 +0200

    modified fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -11,3 +12,33 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// 
+mysql
+  .createConnection({
+    host: 'localhost',
+    database: 'empleados',
+    user: 'root',
+    password: 'tuPassword',
+  })
+  .then(connection => {
+    connection
+      .connect()
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      })
+      .then(() => {
+        return connection.query('SELECT * FROM empleados');
+      })
+      .then(([results, fields]) => {
+        results.forEach((result) => {
+          console.log(result);
+        });
+      })
+      .catch((err) => {
+        console.error('Error en la query: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
